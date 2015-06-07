3c57bb014d21eb30f2cc2668982143bb3a466597
shelbyi <tommy.griese@gmail.com>
2015-06-07 03:28:26 +0200

    renamed alogdat to algodat

--- GARE/src/test/java/net/hft/algodat/test/TournamentSelectionTest.java
+++ GARE/src/test/java/net/hft/algodat/test/TournamentSelectionTest.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.hft.alogdat.test;
+package net.hft.algodat.test;
 
 import java.util.ArrayList;
 import java.util.List;
-import net.hft.alogdat.selectionmethods.TournamentSelection;
+
+import net.hft.algodat.selectionmethods.TournamentSelection;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
